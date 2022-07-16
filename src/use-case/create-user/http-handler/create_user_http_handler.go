4289159548ecd41431db61a2f21656e043281dd2
ellekrau <ellekrau91@gmail.com>
2022-07-15 22:48:50 -0300

    Add create-user request object creation

--- src/use-case/create-user/http-handler/create_user_http_handler.go
+++ src/use-case/create-user/http-handler/create_user_http_handler.go
@@ -1,11 +1,19 @@
 package createuserhttphandler
 
 import (
+	"fmt"
 	"github.com/ellekrau/mercafacil/use-case/create-user/http-handler/contracts"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func CreateUserHttpHandler(c *gin.Context) {
+	request, err := contracts.CreateRequest(c)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, contracts.NewResponse(err.Error()))
+		return
+	}
+	fmt.Sprint(request)
+
 	c.JSON(http.StatusNotImplemented, contracts.NewResponse("Hello world!"))
 }
