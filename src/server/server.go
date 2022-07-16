735600574bd5c63a32536eed1f0106124094e952
ellekrau <ellekrau91@gmail.com>
2022-07-15 22:22:27 -0300

    Add router and add create-user route

--- src/server/server.go
+++ src/server/server.go
@@ -1,1 +1,20 @@
 package server
+
+import (
+	createuserhttphandler "github.com/ellekrau/mercafacil/use-case/create-user/http-handler"
+	"github.com/gin-gonic/gin"
+	"log"
+)
+
+func RunServer() {
+	// Logger and recovery middlewares attached by default
+	router := gin.Default()
+
+	router.POST("/user", createuserhttphandler.CreateUserHttpHandler)
+
+	// TODO Pass port like ":3300"
+	// TODO Get port from .env
+	if err := router.Run(); err != nil {
+		log.Fatalln("run router error: ", err.Error())
+	}
+}
--- src/use-case/create-user/http-handler/create_user_http_handler.go
+++ src/use-case/create-user/http-handler/create_user_http_handler.go
@@ -1,3 +1,11 @@
-package httphandler
+package createuserhttphandler
 
+import (
+	"github.com/ellekrau/mercafacil/use-case/create-user/http-handler/contracts"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
+func CreateUserHttpHandler(c *gin.Context) {
+	c.JSON(http.StatusNotImplemented, contracts.NewResponse("Hello world!"))
+}
