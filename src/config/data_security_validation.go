c0d1ca7fbd8f2b17b7857b2be477b8d758d5f0aa
Adrielle <ellekrau91@gmail.com>
2022-07-17 16:41:04 -0300

    Comment data security implementation

--- src/config/data_security_validation.go
+++ src/config/data_security_validation.go
@@ -1,11 +1,12 @@
 package config
 
-import "errors"
-
-func validateDataSecurityKey() error {
-	if len(DataSecurity.SecurityDataKey) != 16 {
-		return errors.New("data security key must have 16 chars")
-	}
-
-	return nil
-}
+//
+//import "errors"
+//
+//func validateDataSecurityKey() error {
+//	if len(DataSecurity.SecurityDataKey) != 16 {
+//		return errors.New("data security key must have 16 chars")
+//	}
+//
+//	return nil
+//}
