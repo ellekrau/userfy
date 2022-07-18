3cf6cbcb46d7fdf7ed452a5325debbf8065500ea
Adrielle <ellekrau91@gmail.com>
2022-07-18 11:33:47 -0300

    Alter generate-token error result to customerror pattern

--- src/use-case/get-jwt-token/http-handler/get_jwt_token _http_handler.go
+++ src/use-case/get-jwt-token/http-handler/get_jwt_token _http_handler.go
@@ -3,6 +3,7 @@ package http_handler
 import (
 	"github.com/ellekrau/mercafacil/server/middlewares/auth/jwt"
 	"github.com/ellekrau/mercafacil/use-case/get-jwt-token/http-handler/contracts"
+	customerror "github.com/ellekrau/mercafacil/utils/custom-error"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -12,7 +13,10 @@ func GenerateJWT(c *gin.Context) {
 
 	jwtToken, err := jwt.GenerateJWTToken()
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, contracts.NewResponse("Generate token error: "+err.Error()))
+		c.JSON(http.StatusInternalServerError, customerror.CustomError{
+			Code:    "generate_token",
+			Message: "Generate token error: " + err.Error(),
+		})
 		return
 	}
 
