27f8756eb719e27a624ddf89864fd8bb821dcf98
Adrielle <ellekrau91@gmail.com>
2022-07-23 16:45:07 -0300

    Add IUserRepository

--- src/database/repositories/user-repository/user_repository_postgres.go
+++ src/database/repositories/user-repository/user_repository_postgres.go
@@ -2,10 +2,24 @@ package userrepository
 
 import (
 	"database/sql"
+	clientconfig "github.com/ellekrau/userfy/config/client-config"
 	"github.com/ellekrau/userfy/domain"
 )
 
-func createUserPostgres(db *sql.DB, user *domain.User) (err error) {
-	err = db.QueryRow("INSERT INTO contacts (nome, celular) VALUES($1, $2) RETURNING id;", user.Name, user.Cellphone).Scan(&user.ID)
+type postgresUserRepository struct {
+	userRepository
+}
+
+func newPostgresUserRepository(clientConfig clientconfig.Client, db *sql.DB) *postgresUserRepository {
+	return &postgresUserRepository{
+		userRepository{
+			clientDBConfig: clientConfig.Database,
+			db:             db,
+		},
+	}
+}
+
+func (mur postgresUserRepository) CreateUser(user *domain.User) (err error) {
+	err = mur.db.QueryRow("INSERT INTO contacts (nome, celular) VALUES($1, $2) RETURNING id;", user.Name, user.Cellphone).Scan(&user.ID)
 	return
 }
